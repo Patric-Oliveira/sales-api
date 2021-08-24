94367f168e65197bb752cbcb0ea2484bc1341858
Patric Oliveira <patric.eng.computation@gmail.com>
2021-08-24 13:24:48 -0300

    product module features

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
@@ -11,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof AppError) {
@@ -26,5 +30,5 @@ app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
 });
 
 app.listen(8000, () => {
-  console.log('Server started on port 3333');
+  console.log('Server started on port 8000');
 });
